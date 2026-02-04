df86ce732547a22edd001ea97a324c54e5cb02b6
skppraktisnakes <skppraktis@gmail.com>
2026-02-04 23:56:36 +0700

    first

---
+++ sw.js
@@ -0,0 +1,45 @@
+﻿// Service Worker untuk SKP Praktis Nakes - Daerah Page
+const CACHE_NAME = 'skp-praktis-daerah-v2';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/daerah.html',
+    '/css/style.css',
+    '/js/script.js',
+    '/js/daerah-enhanced.js',
+    '/assets/icons/favicon.ico'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
